b5d55c830f20b6503a21deff2558d044c03b9736
Ivo Lima <ivo.romario@gmail.com>
2016-02-02 16:40:03 +0100

    initial commit

---
+++ exporters/__init__.py
@@ -0,0 +1,3 @@
+from .classes import Spotify
+
+__all__ = ['Spotify']
---
+++ exporters/classes.py
@@ -0,0 +1,18 @@
+
+
+
+class Spotify:
+    """
+    """
+
+    def __init__(self):
+        """
+        """
+        pass
+
+    def authorize(self, client_id):
+        """
+        """
+        pass
+
+    def
